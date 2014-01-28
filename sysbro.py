1574dd684603aa259485d35eb261ac50cdba94e3
Dop <dopheide@gmail.com>
2014-01-27 20:34:12 -0600

    Fixed indents... stupid Python.

--- sysbro.py
+++ sysbro.py
@@ -28,12 +28,12 @@ class SyslogUDPHandler(SocketServer.BaseRequestHandler):
         print( "%s : " % self.client_address[0], str(data))
         logging.info(str(data))
  
-        if __name__ == "__main__":
-            try:
-                server = SocketServer.UDPServer((HOST,PORT), SyslogUDPHandler)
-                server.serve_forever(poll_interval=0.5)
-            except (IOError, SystemExit):
-                raise
-            except KeyboardInterrupt:
-                print ("Crtl+C Pressed. Shutting down.")
-                
+if __name__ == "__main__":
+    try:
+        server = SocketServer.UDPServer((HOST,PORT), SyslogUDPHandler)
+        server.serve_forever(poll_interval=0.5)
+    except (IOError, SystemExit):
+        raise
+    except KeyboardInterrupt:
+        print ("Crtl+C Pressed. Shutting down.")
+
